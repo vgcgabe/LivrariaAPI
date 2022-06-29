b3e905ca37d32cbc2a94a036f2aec1bbf1c01b75
João Vitor <joaovitoraraujo1821@gmail.com>
2022-06-28 21:45:14 -0300

    Adicionei as validações de atributos do ClienteModel

--- src/model/ClienteModel.js
+++ src/model/ClienteModel.js
@@ -6,24 +6,53 @@ export class ClienteModel {
         this._cpf    =  this.validaCpf(cpf);
     };
 
-    static formataNome(nome) {
-        return;
-    };
-
-    static validaCpf(cpf) {
-        return;
-    };
-
     static validaEmail(email) {
-        return;
+        const regex = new RegExp("^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$");
+
+        if (regex.test(email)) {
+            return email;
+        } else {
+            throw new Error('Email inválido!');
+        };
     };
 
     static validaNumero(numero) {
-        return;
+        for (i of numero) {
+            if (i == '-' || i == '(' || i == ')') {
+                numero = numero.replace(i, '');
+            };
+        };
+
+        if (numero.length != 11) {
+            throw new Error('Número inválido!');
+        } else {
+            const regex = new RegExp("[1-9]{2}9[0-9]{4}[0-9]{4}");
+
+            if (regex.test(numero)) {
+                return numero
+            };
+        };
     };
 
     static validaCpf(cpf) {
-        return;
+        for (i of cpf) {
+            if (i == '.' || i == '-') {
+                cpf = cpf.replace(i, '');
+            };
+        };
+        
+        if (cpf.length != 11) {
+            throw new Error('CPF inválido!');
+            
+        } else {
+            const regex = new RegExp('([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})');
+
+            if (regex.test(cpf)) {
+                return cpf;
+            } else {
+                throw new Error('CPF inválido!');
+            };
+        };
     };
 
     get nome() {
