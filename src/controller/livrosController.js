aa75d4829897535898ae2f70137707ce77e55e43
MagaliLimaDev <mahrgfm@gmail.com>
2022-07-01 19:31:22 -0300

    update folder livro model/controller

---
+++ src/controller/livrosController.js
@@ -0,0 +1,26 @@
+import Livros from "../model/LivrosModel"
+import express from "express";
+const app = express ();
+app.use(express.json());
+
+
+
+app.get("/livros", (req, res)=>{
+    
+});
+
+app.get("/livros/id", (req, res)=>{
+    
+});
+
+app.post("/livros", (req, res)=>{
+    
+});
+
+app.put("/livros", (req, res)=>{
+    
+});
+
+app.delete("/livros", (req, res)=>{
+    
+});
---
+++ src/model/LivrosModel.js
@@ -0,0 +1,11 @@
+class Livros{
+    constructor(id, titulo, autor, editora, preço){
+        this.id = id
+        this.titulo = titulo
+        this.autor = autor
+        this.editora = editora
+        this.preço = preço
+    }
+}
+
+export default Livros
