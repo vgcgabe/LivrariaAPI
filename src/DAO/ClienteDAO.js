a443fe7a8bb2baeecacaf38c78f574a5331930fb
João Vitor <joaovitoraraujo1821@gmail.com>
2022-07-01 21:12:25 -0300

    Implementei o GET, DELETE e o POST no DAO

--- src/DAO/ClienteDAO.js
+++ src/DAO/ClienteDAO.js
@@ -9,7 +9,60 @@ export class ClienteDAO {
                 if (error) {
                     reject(`Erro ao selecionar a tabela: ${error.message}`);
                 } else {
-                    resolve({"TABLE": result});
+                    resolve({
+                        "TABLE": result
+                    });
+                };
+            });
+        });
+    };
+
+    getClientesById(cpf) {
+        return new Promise((resolve, reject) => {
+            this.bd.all(`SELECT NOME FROM CLIENTES WHERE CPF='${cpf}'`, (error, result) => {
+                if (error) {
+                    reject(`Erro ao procurar o usuário: ${error.message}`)
+                } else {
+                    resolve({
+                        "USUARIO": result
+                    });
+                };
+            });
+        });
+    };
+
+    deleteCliente(cpf) {
+        return new Promise((resolve, reject) => {
+            this.bd.all(`DELETE FROM CLIENTES WHERE CPF='${cpf}'`, (error, result) => {
+                if (error) {
+                    reject(`Erro ao deletar o usuário: ${error.message}`)
+                } else {
+                    resolve({
+                        "USUARIO DELETADO": result
+                    });
+                };
+            });
+        });
+    };
+
+    postCliente(cliente) {
+        return new Promise((resolve, reject) => {
+            this.bd.run(`
+            UPDATE Clientes SET
+            nome=?, sobrenome=?, cpf=?, email=?, numero=?
+            WHERE id=?`, [
+                cliente.nome,
+                cliente.cpf,
+                cliente.email,
+                cliente.numero,
+                id
+            ], (error, result) => {
+                if (error) {
+                    reject(`Erro ao inserir o usuário: ${error}`)
+                } else {
+                    resolve({
+                        "USUARIO ADICIONADO": result
+                    });
                 };
             });
         });
@@ -17,5 +70,5 @@ export class ClienteDAO {
 
     get bd() {
         return this._bd;
-    }
-}
+    };
+};
