1520007c48a9f0bbae6759f213c1b64ee51142db
vgcgabe <v.g.cguedes@gmail.com>
2022-07-05 17:50:30 -0300

    Todas as rotas de 'Funcionários' estão funcionando

--- src/app.js
+++ src/app.js
@@ -2,13 +2,13 @@ import  Express  from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 
-import { clienteController } from "./controller/clienteController.js";
-import { departamentoController } from "./controller/departamentoController.js";
-import { fornecedorController } from "./controller/fornecedorController.js"
-import { funcionariosController } from "./controller/funcionariosController.js";
-import { livrosController } from "./controller/livrosController.js";
+//import { clienteController } from "./controller/clienteController.js";
+//import { departamentoController } from "./controller/departamentoController.js";
+//import { fornecedorController } from "./controller/fornecedorController.js"
+import { funcionarios } from "./controller/funcionariosController.js";
+//import { livrosController } from "./controller/livrosController.js";
 
-import { bd } from "./infra/configDB.js";
+//import { bd } from "./infra/configDB.js";
 
 const app = Express();
 const port = 3000;
@@ -16,12 +16,20 @@ const port = 3000;
 app.use(bodyParser.json());
 app.use(cors());
 
-clienteController(app, bd);
-departamentoController(app, bd);
-fornecedorController(app, bd);
-funcionariosController(app, bd);
-livrosController(app, bd);
+// clienteController(app, bd);
+// departamentoController(app, bd);
+// fornecedorController(app, bd);
+funcionarios(app);
+// livrosController(app, bd);
 
-app.listen(port, () => {
-    console.log(`Listen to port ${port}: http://localhost:3000/cliente`);
-});
+// app.listen(port, () => {
+//     console.log(`Listen to port ${port}: http://localhost:3000/cliente`);
+// });
+
+app.get('/', (req, res) => {
+    res.send("Rota principal")
+  });
+  
+  app.listen(port, ()=> {
+    console.log(`Rodando na porta ${port}`)
+  });
