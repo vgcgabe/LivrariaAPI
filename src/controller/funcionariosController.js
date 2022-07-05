221e2b087079dc17a420068239935fd54c4504e3
vgcgabe <v.g.cguedes@gmail.com>
2022-07-04 22:49:58 -0300

    Todas as rotas de 'Funcionários' estão funcionando

---
+++ src/controller/funcionariosController.js
@@ -0,0 +1,23 @@
+//Funcionários - Victor Gabriel Camargo Guedes
+export const funcionarios = (app) => {
+    //ROTA PARA CONSULTAR FUNCIONARIOS SEM PARAMETRO
+    app.get('/funcionarios', (req, res)=>{
+        res.send("ROTA GET FUNCIONARIOS");
+    })
+    //ROTA PARA CONSULTAR FUNCIONARIOS POR PARAMETRO
+    app.get('/funcionarios/:id', (req, res)=>{
+        res.send("ROTA GET FUNCIONARIOS POR PARAMETRO");
+    })
+    //ROTA PARA CADASTRAR FUNCIONARIOS
+    app.post('/funcionarios', (req, res)=>{
+        res.send("ROTA POST FUNCIONARIOS");
+    })
+    //ROTA PARA ALTERAR   FUNCIONARIOS
+    app.put('/funcionarios', (req, res)=>{
+        res.send("ROTA PUT FUNCIONARIOS");
+    })
+    //ROTA PARA DELETAR   FUNCIONARIOS
+    app.delete('/funcionarios', (req, res)=>{
+        res.send("ROTA DELETE FUNCIONARIOS");
+    })  
+}
