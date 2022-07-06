0fac1e6038dfe5ca6a2da20affd0395e16922901
vgcgabe <v.g.cguedes@gmail.com>
2022-07-05 22:20:37 -0300

    Criei a database da entidade Funcionários

--- src/model/FuncionarioModel.js
+++ src/model/FuncionarioModel.js
@@ -7,23 +7,23 @@ export class Funcionario{
         this.email = email;
     };
 
-    // get nome() {
-    //     return this.nome;
-    // };
+    get nome() {
+        return this.nome;
+    };
 
-    // get id() {
-    //     return this.id;
-    // };
+    get id() {
+        return this.id;
+    };
 
-    // get cpf() {
-    //     return this.cpf;
-    // };
+    get cpf() {
+        return this.cpf;
+    };
 
-    // get idade() {
-    //     return this.idade;
-    // };
+    get idade() {
+        return this.idade;
+    };
 
-    // get email() {
-    //     return this.email;
-    // };
+    get email() {
+        return this.email;
+    };
 };
